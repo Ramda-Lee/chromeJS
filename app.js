d06bf00a0870f70ed47551bc285909aafa736b67
이남대 <inamdae@inamdaeui-MacBookPro.local>
2024-06-23 00:46:07 +0900

    Make greeting function

--- app.js
+++
@@ -1,9 +0,0 @@
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
-
-function onLoginBtnClick(){
-    console.dir(loginInput.value);
-    console.log("click~");
-}
-
-loginButton.addEventListener("click" ,onLoginBtnClick);
