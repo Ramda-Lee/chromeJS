013fba072a59f07339c440361c0f6f72d9449c55
이남대 <inamdae@inamdaeui-MacBookPro.local>
2024-06-25 00:33:09 +0900

    Complete JS Study

--- js/background.js
+++ js/background.js
@@ -1,6 +1,3 @@
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
-const chosenImages = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImages}`;
-document.body.appendChild(bgImage);
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+document.body.style.backgroundImage = `url('img/${chosenImage}')`;
